7fbbe8990b8f040e3f124c631eb3688777f68a16
mohand <mohanadalnjjar23@gmail.com>
2024-05-17 04:44:00 +0300

    allow user to save new model to the firebase

---
+++ lib/core/utilities/services/save_news_to_fire_store.dart
@@ -0,0 +1,26 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:pressWave/home/data/models/news_model.dart';
+
+Future<void> saveNewOnfireStore({required NewsModel newsModel}) async {
+  final FirebaseAuth auth = FirebaseAuth.instance;
+  final User? user = auth.currentUser;
+  final userDb = FirebaseFirestore.instance.collection('users').doc(user!.uid);
+  await userDb.update(
+    {
+      'UserSavedNews': FieldValue.arrayUnion(
+        [
+          {
+            'author': newsModel.author,
+            'title': newsModel.title,
+            'description': newsModel.description,
+            'url': newsModel.url,
+            'urlToImage': newsModel.urlToImage,
+            'publishedAt': newsModel.publishedAt,
+            'content': newsModel.content,
+          },
+        ],
+      )
+    },
+  );
+}
--- lib/home/presentation/views/news_details_view.dart
+++ lib/home/presentation/views/news_details_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:iconly/iconly.dart';
+import 'package:pressWave/core/functions/showed_scaffold_message.dart';
+import 'package:pressWave/core/utilities/services/save_news_to_fire_store.dart';
 import 'package:pressWave/core/utilities/styles.dart';
 import 'package:pressWave/home/data/models/news_model.dart';
 import 'package:pressWave/home/presentation/widgets/news_details_view_body.dart';
@@ -7,6 +9,7 @@ import 'package:pressWave/home/presentation/widgets/news_details_view_body.dart'
 class NewsDetailsView extends StatelessWidget {
   const NewsDetailsView({super.key, required this.newsModel});
   final NewsModel newsModel;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +20,16 @@ class NewsDetailsView extends StatelessWidget {
         ),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () async {
+              try {
+                await saveNewOnfireStore(newsModel: newsModel);
+              } catch (e) {
+                showedScaffoldMessage(
+                  context: context,
+                  message: e.toString(),
+                );
+              }
+            },
             icon: const Icon(
               IconlyLight.bookmark,
             ),
